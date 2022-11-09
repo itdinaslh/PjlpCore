547782674a2e2d3d4f535fe724361eea160254c8
itdinaslh <itdlh@outlook.com>
2022-11-09 23:58:40 +0700

    Update Lokasi Kerja

---
+++ Controllers/Master/LokasiKerjaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PjlpCore.Controllers;
+
+public class LokasiKerjaController : Controller
+{
+    [HttpGet("/master/lokasi-kerja")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Domain/Entities/Master/LokasiKerja.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PjlpCore.Entities;
+
+[Table("lokasi_kerja")]
+public class LokasiKerja
+{
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int LokasiKerjaID { get; set; }
+
+#nullable disable
+
+    public string NamaLokasi { get; set; }
+
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+}
