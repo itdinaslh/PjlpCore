729eca48094b0dc36eb56bf8bbe3815b3843fd0f
itdinaslh <itdlh@outlook.com>
2022-11-22 19:30:22 +0700

    Add Annotations

--- Domain/Entity/Main/FilePegawai.cs
+++ Domain/Entity/Main/FilePegawai.cs
@@ -1,38 +1,38 @@
-﻿using PjlpCore.Entity;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PjlpCore.Entity
+namespace PjlpCore.Entity;
+
+[Table("filepegawais")]
+public class FilePegawai
 {
-    public class FilePegawai
-    {
-        [Key]
-        public Guid FilePegawaiID { get; set; } = Guid.Empty;
+    [Key]
+    public Guid FilePegawaiID { get; set; } = Guid.Empty;
 
 #nullable disable
 
-        public Guid PegawaiID { get; set; }
+    public Guid PegawaiID { get; set; }
 
-        public int PersyaratanID { get; set; }
+    public int PersyaratanID { get; set; }
 
-        [Required]
-        [MaxLength(255)]
-        public string FileName { get; set; }
+    [Required]
+    [MaxLength(255)]
+    public string FileName { get; set; }
 
-        [DataType(DataType.Text)]
-        public string RealName { get; set; }
+    [DataType(DataType.Text)]
+    public string RealName { get; set; }
 
-        [DataType(DataType.Text)]
-        public string FilePath { get; set; }
+    [DataType(DataType.Text)]
+    public string FilePath { get; set; }
 
-        [MaxLength(10)]
-        public string FileExtension { get; set; }
+    [MaxLength(10)]
+    public string FileExtension { get; set; }
 
-        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? CreatedAt { get; set; } = DateTime.Now;
 
-        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; } = DateTime.Now;
 
-        public Pegawai Pegawai { get; set; }
+    public Pegawai Pegawai { get; set; }
 
-        public Persyaratan Persyaratan { get; set; }
-    }
+    public Persyaratan Persyaratan { get; set; }
 }
