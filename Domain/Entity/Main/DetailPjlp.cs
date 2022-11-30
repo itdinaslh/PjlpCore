a483fc2c22e0b0c3a4960377449dbbbc869e2f6b
itdinaslh <itdlh@outlook.com>
2022-12-01 04:06:51 +0700

    Update DetailPJLP

--- Domain/Entity/Main/DetailPjlp.cs
+++ Domain/Entity/Main/DetailPjlp.cs
@@ -1,38 +1,39 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PjlpCore.Entity
+namespace PjlpCore.Entity;
+
+[Table("detailpjlps")]
+public class DetailPjlp
 {
-    public class DetailPjlp
-    {
-        [Key]
-        public Guid DetailPjlpID { get; set; }
+    [Key]
+    public Guid DetailPjlpID { get; set; }
 
-        public Guid PegawaiID { get; set; }
+    public Guid PegawaiID { get; set; }
 
-        [MaxLength(5)]
-        public string? Tanggungan { get; set; }
+    [MaxLength(5)]
+    public string? Tanggungan { get; set; }
 
-        [MaxLength(50)]
-        public string? NoBPJSK { get; set; }
+    [MaxLength(50)]
+    public string? NoBPJSK { get; set; }
 
-        [MaxLength(30)]
-        public string? NoSIM { get; set; }
+    [MaxLength(30)]
+    public string? NoSIM { get; set; }
 
-        public DateOnly? MasaBerlakuSIM { get; set; }
+    public DateOnly? MasaBerlakuSIM { get; set; }
 
-        public Guid? JabatanID { get; set; }
+    public Guid? JabatanID { get; set; }
 
-        public DateTime? CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
-        public DateTime? UpdatedAt { get;}
+    public DateTime? UpdatedAt { get;}
 
 #nullable disable
 
-        public Pegawai Pegawai { get; set; }
+    public Pegawai Pegawai { get; set; }
 
 #nullable enable
 
-        public Jabatan? Jabatan { get; set; }
+    public Jabatan? Jabatan { get; set; }
 
-    }
 }
