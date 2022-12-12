1a2867c04586a4d62e73d022f8218b8798356f2a
itdinaslh <itdlh@outlook.com>
2022-12-12 20:03:45 +0700

    Date Checker

---
+++ Controllers/ManageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PjlpCore.Controllers;
+
+public class ManageController : Controller
+{
+    [HttpGet("/manage/check/date")]
+    public DateTime CheckDate()
+    {
+        return DateTime.Now;
+    }
+}
